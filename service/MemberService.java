659e5f594443e7c7c9414b9df06591dd5e5e55d9
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2025-06-25 11:53:21 +0900

    service

---
+++ service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service // Spring에게 service임을 알림
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     *  회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) // Optional을 통해 null 그 자체도 반환 가능
+            .ifPresent(m -> { // 동일한 이름이 있으면 다음 코드 실행
+                throw new IllegalStateException("이미 존재하는 회원입니다."); // 현재 상태가 잘못되었음을 나타내는 예외
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
